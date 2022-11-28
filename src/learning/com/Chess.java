3a6ca0a6782d9dc1971817d3f77256e5769284b5
Kawaragi <dannyboykov@bk.ru>
2022-11-28 17:49:21 +0300

    Задачка про Шахматы.

---
+++ src/learning/com/Chess.java
@@ -0,0 +1,17 @@
+package learning.com;
+
+import java.util.Scanner;
+
+public class Chess {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        int ver = vvod.nextInt();
+        int gor = vvod.nextInt();
+        int[][] m = new int[8][8];
+        for (int i = 0; i <= m.length; i++){
+            for (int  j = 0; j<= m[0].length; j++){
+            }
+        }
+
+    }
+}
