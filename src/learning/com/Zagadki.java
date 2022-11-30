54ff409a7e77febb8035bbdebe17a3749f3a57b5
Daniil <dannyboykov@bk.ru>
2022-11-30 23:53:59 +0300

    Zagadki.

--- src/learning/com/Zagadki.java
+++ src/learning/com/Zagadki.java
@@ -1,2 +1,26 @@
-package learning.com;public class Zagadki {
+package learning.com;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zagadki {
+    public static void main(String[] args) {
+        int numb;
+        String vvodd;
+        Scanner vvod = new Scanner(System.in);
+        Random random = new Random();
+        String[] zag = {"Висит груша нельзя скушать", "Зимой и летом одним цветом"};
+        String[] otv = {"Лампочка", "Елка"};
+        numb = random.nextInt(2);
+        System.out.println(zag[numb]);
+        do {
+            vvodd = vvod.nextLine();
+            if (!otv[numb].equals(vvodd)) {
+                System.out.println("Подумай еще");
+            }
+        }
+        while (!vvodd.equals(otv[numb]));
+        System.out.println("Right");
+    }
 }
+//norm tema
