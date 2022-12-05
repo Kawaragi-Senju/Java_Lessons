b09d09b6dabdf4b120599b24f1c4e4c366ca002d
Daniil <dannyboykov@bk.ru>
2022-12-05 16:06:18 +0300

    Evkl_Al Commit with console

--- src/learning/com/Evkld_Alg.java
+++ src/learning/com/Evkld_Alg.java
@@ -1,7 +1,20 @@
 package learning.com;
 
+import java.util.Scanner;
+
 public class Evkld_Alg {
     public static void main(String[] args) {
-
+        Scanner vvod = new Scanner(System.in);
+        System.out.println("Введите два числа");
+        int x = vvod.nextInt();
+        int y = vvod.nextInt();
+        while ((x != 0) & (y != 0)){
+            do if (x > y) {
+                x = x % y;
+            } else {
+                y = y % x;
+            }while(false);
+        }
+        System.out.println(x + y);
     }
 }
