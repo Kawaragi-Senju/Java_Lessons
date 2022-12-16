d97c92ac3632d5dc1d70b65004a838f394714316
Kawaragi <dannyboykov@bk.ru>
2022-12-16 19:48:38 +0300

    Делители правка.

--- src/learning/com/Deliteli.java
+++ src/learning/com/Deliteli.java
@@ -8,10 +8,11 @@ public class Deliteli {
         System.out.println("Введем число");
         int z = vvod.nextInt();
         chisla(z);
+
     }
 
     static boolean chisla(int n) {
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 System.out.println(i);
             }
