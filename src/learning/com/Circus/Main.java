47a1cd34e458510ec2009b77c468057730bf825f
Daniil <dannyboykov@bk.ru>
2022-12-20 16:56:03 +0300

    A little bit tougher than before.

--- src/learning/com/Circus/Main.java
+++ src/learning/com/Circus/Main.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите радиус круга: ");
         Scanner vvod = new Scanner(System.in);
         Circus odin = new Circus(vvod.nextInt());
-        System.out.println(odin.showsquare());
+        String result = String.format(("%.1f"), odin.showsquare());
+        System.out.println(result);
      }
 }
 //1. Создать класс Circus (Круг) с полем square (площадь) конструктор которого принимает
