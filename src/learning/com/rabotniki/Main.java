45833fc9f6fd3622e42d49cab3205dac00fd38b3
Daniil <dannyboykov@bk.ru>
2022-12-21 22:12:51 +0300

    prosto tak.

--- src/learning/com/rabotniki/Main.java
+++ src/learning/com/rabotniki/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число часов ");
-        Rabotnik ch = new Rabotnik(4, 5);
         System.out.println(Rabotnik.zp(scanner.nextInt()));
-        System.out.println(ch);
     }
 }
--- src/learning/com/rabotniki/Rabotnik.java
+++ src/learning/com/rabotniki/Rabotnik.java
@@ -3,6 +3,7 @@ package learning.com.rabotniki;
 public class Rabotnik {
     int koef;
     int hourlyrate;
+    int time1 = 10;
     static int vkof = 10;
 
     public Rabotnik(int koef, int hourlyrate){
@@ -10,7 +11,7 @@ public class Rabotnik {
         this.hourlyrate = hourlyrate;
     }
 
-    public int zarplata(int time1){
+    public int zarplata(){
         return hourlyrate * time1 + koef;
     }
     public static int zp(int time){
