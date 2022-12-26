a769dc4b684cf4d5ea406664b5b1747e22f448f1
Kawaragi <dannyboykov@bk.ru>
2022-12-26 14:29:24 +0300

    car.

--- src/learning/com/car/Car.java
+++ src/learning/com/car/Car.java
@@ -1,9 +1,23 @@
 package learning.com.car;
 
 public class Car {
-    static byte tankVolume = 100;
-    byte remaningFuel = tankVolume;
+    final static byte tankVolume = 100;
+    static byte remaningFuel;
 
+    public Car(byte remaningFuel){
+        this.remaningFuel = tankVolume;
+    }
+    public static double spendFuel(byte drived){
+        return (drived);
+    }
+
+    public static byte showRemaningFuel(byte remaningFuel, byte drived){
+        return (byte) (remaningFuel - drived);
+    }
+
+    public static byte showTankVolume() {
+        return tankVolume;
+    }
 }
 //Создать класс Car cо статической переменной  tankVolume (объем бака).  Присвоить переменной значение 100. Определить в классе нестатическую переменную remainingFuel (оставшееся топливо) изначально равное tankVolume (присвоить эту величину внутри конструктора) . Определить методы:
 //Нестатический spendFuel( double spendedFuel),  «расходующий» топливо из оставшегося объема.
