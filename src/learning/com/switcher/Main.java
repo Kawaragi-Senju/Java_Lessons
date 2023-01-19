899eef673a06e67423891cc62725c527b15d2017
Daniil <dannyboykov@bk.ru>
2023-01-19 12:02:47 +0300

    From lesson.

--- src/learning/com/switcher/Main.java
+++ src/learning/com/switcher/Main.java
@@ -3,9 +3,10 @@ package learning.com.switcher;
 public class Main {
     public static void main(String[] args) {
         Switcher switcher = new Switcher();
+        Computer computer = new Computer();
         Tv tv = new Tv();
         MusicBox musicBox = new MusicBox();
-        switcher.setTech(tv);
+        switcher.setTech(computer);
         switcher.turnOnTechnic();
     }
 }
