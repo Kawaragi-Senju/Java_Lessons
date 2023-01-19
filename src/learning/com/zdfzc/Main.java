77dab8f7694ea3ad086091e04097178086bbab98
Daniil <dannyboykov@bk.ru>
2023-01-19 12:03:03 +0300

    From lesson.

---
+++ src/learning/com/zdfzc/Main.java
@@ -0,0 +1,11 @@
+package learning.com.zdfzc;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    static int calculate(Month ... months){
+        Month[] a = Month.values();
+        return 0;
+    }
+}
---
+++ src/learning/com/zdfzc/Month.java
@@ -0,0 +1,16 @@
+package learning.com.zdfzc;
+
+public enum Month {
+    DECEMBER(70),
+    JANUARY(70),
+    FEBRUARY(70),
+    SEPTEMBER(120),
+    OCTOBER(120),
+    NOVEMBER(120);
+
+    int zp;
+
+     Month(int zp){
+        this.zp = zp;
+    }
+}
