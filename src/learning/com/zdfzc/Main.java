a5e1e185cf8f88e9ce1753a7e3d35306d34271e1
Daniil <dannyboykov@bk.ru>
2023-01-26 21:06:25 +0300

    enum.

--- src/learning/com/zdfzc/Main.java
+++ src/learning/com/zdfzc/Main.java
@@ -2,10 +2,27 @@ package learning.com.zdfzc;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(calculate(Month.JANUARY, Month.SEPTEMBER, Month.FEBRUARY));
     }
     static int calculate(Month ... months){
+        int p = 0;
         Month[] a = Month.values();
-        return 0;
+        for (Month month : a) {
+            if (!searchMounth(month, months)) {
+                System.out.println(month);
+            }else{
+                p += month.zp;
+            }
+        }
+        return p;
+    }
+
+    public static boolean searchMounth(Month month, Month[] months){
+        for (Month value : months) {
+            if (month == value) {
+                return true;
+            }
+        }
+        return false;
     }
 }
