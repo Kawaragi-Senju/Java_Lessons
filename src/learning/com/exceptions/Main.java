56305acc9583e623b7c51670de7f664cf181fe13
Daniil <dannyboykov@bk.ru>
2023-02-23 21:04:06 +0300

    /

---
+++ src/learning/com/exceptions/Main.java
@@ -0,0 +1,24 @@
+package learning.com.exceptions;
+
+public class Main {
+    public static void main(String[] args) {
+        foo();
+    }
+
+    static String metod(String str) throws Exception{
+        if (str != null){
+            return "Hello " + str;
+        }
+        throw new EmptyNameExeption();
+    }
+
+    static void foo(){
+        try{
+            System.out.println(metod("sfdf"));
+        }catch(Exception e){
+            System.out.println("Hello Noname");
+        }finally {
+            System.out.println("Program is over");
+        }
+    }
+}
