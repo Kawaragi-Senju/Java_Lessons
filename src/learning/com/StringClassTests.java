b8ccc11d4186fca9d490c170d6002469fa0f8463
Daniil <dannyboykov@bk.ru>
2023-02-25 16:18:59 +0300

    /

--- src/learning/com/StringClassTests.java
+++ src/learning/com/StringClassTests.java
@@ -1,8 +1,8 @@
 package learning.com;
 
-//public class StringClassTests {
-//    public static void main(String[] args) {
-//        String string = "adfs";
-//        System.out.println(string.substring());
-//    }
-//}
+public class StringClassTests {
+    public static void main(String[] args) {
+        String string = "adfs";
+        System.out.println(string.length());
+    }
+}
