a412c67987920e0454204633d25af45f436c6979
Daniil <dannyboykov@bk.ru>
2023-02-25 16:18:18 +0300

    /

---
+++ src/learning/com/exceptiondz/IllegalOperatorException.java
@@ -0,0 +1,7 @@
+package learning.com.exceptiondz;
+
+public class IllegalOperatorException extends Exception{
+     public IllegalOperatorException(String message){
+         super(message);
+     }
+}
