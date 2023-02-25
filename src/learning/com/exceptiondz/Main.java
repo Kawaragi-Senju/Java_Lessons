45e02c4101b6784e1ac8f54628a00af67c831360
Daniil <dannyboykov@bk.ru>
2023-02-25 16:18:38 +0300

    /

---
+++ src/learning/com/exceptiondz/Main.java
@@ -0,0 +1,51 @@
+package learning.com.exceptiondz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите выражение: ");
+        String a = scanner.nextLine();
+        try{
+            calculate(a);
+        }catch ()
+    }
+
+    private static void calculate(String a) throws LengthException, NumberFormatException, IllegalOperatorException{
+        if(a.length() != 5){
+            throw new LengthException("length != 5");
+        }
+        String s = a.substring(0, 2);
+        String s1 = a.substring(3, 5);
+        String s2 = a.substring(2, 3);
+        if(!s.matches("\\d{2}")){
+            throw new NumberFormatException("Number format is wrong");
+        }
+        if(!s1.matches("\\d{2}")){
+            throw new NumberFormatException("Number format is wrong");
+        }
+        int chis = Integer.parseInt(s);
+        int chis1 = Integer.parseInt(s1);
+        switch (s2){
+            case "*":
+                System.out.println(chis1 * chis);
+                break;
+            case "/":
+                System.out.println(chis1 / chis);
+                break;
+            case "+":
+                System.out.println(chis1 + chis);
+                break;
+            case "-":
+                System.out.println(chis1 - chis);
+                break;
+            default:
+                throw new IllegalOperatorException("IllegalOpertor");
+        }
+
+        int b = Integer.parseInt(a.substring(0, 2));
+    }
+}
+//12*32
