7083a4b60f206efa7b7052212ced54fe3c2f38d5
Daniil <dannyboykov@bk.ru>
2023-05-21 15:31:36 +0300

    /

--- src/learning/com/queue/Account.java
+++ src/learning/com/queue/Account.java
@@ -1,9 +1,33 @@
 package learning.com.queue;
 
 public class Account {
-    int account_number;
-    int cash = 1000000;
-    boolean activity;
+    private int account_number;
+    private int cash = 1000000;
+    private boolean activity;
+
+    public int getAccount_number() {
+        return account_number;
+    }
+
+    public void setAccount_number(int account_number) {
+        this.account_number = account_number;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
+
+    public boolean isActivity() {
+        return activity;
+    }
+
+    public void setActivity(boolean activity) {
+        this.activity = activity;
+    }
 }
 //Счет – это класс, у которого есть следующие поля:
 //        номер счета – целое число
--- src/learning/com/queue/Request.java
+++ src/learning/com/queue/Request.java
@@ -1,11 +1,56 @@
 package learning.com.queue;
 
+import java.util.Calendar;
+
 public class Request {
 
+
+    private int sum;
+    private long numberOfAccount;
+    private Calendar calendar;
+    private RequestType requestType;
+
+    public RequestType getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(RequestType requestType) {
+        this.requestType = requestType;
+    }
+
+
+
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public long getNumberOfAccount() {
+        return numberOfAccount;
+    }
+
+    public void setNumberOfAccount(long numberOfAccount) {
+        this.numberOfAccount = numberOfAccount;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
 }
-//Запрос – это класс, состоящий из следующих полей:
-//        Вид запроса – перечисление со следующими значениями:
+//    Вид запроса – перечисление со следующими значениями:
 //        WRITEDOWN – списание средств со счета
 //        PASSING – зачисление средств на счет
 //        BLOCK – блокирование счета
 //        UNBLOCK – разблокирование счета
+//        Сумма – денежная сумма, которую необходимо списать или зачислить на счет (в зависимости от вида запроса).
+//        Если вид запроса  - BLOCK или UNBLOCK, это поле игнорируется.
+//        Номер счета
+//        Дата создания запроса (включая час, минуты и секунды)
